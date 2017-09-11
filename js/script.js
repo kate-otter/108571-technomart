22cd142babf91a81be9665d8bc25a05559137784
kate-otter <kate.otter@yandex.ru>
2017-09-11 17:10:26 +0500

    Исправлены замечания

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
+
 var mapLink = document.querySelector(".map");
 
 var mapPopup = document.querySelector(".modal-map");
@@ -42,6 +43,39 @@ window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (mapPopup.classList.contains("modal-show")) {
       mapPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+var basketLinks = document.querySelectorAll(".button-buy");
+
+var basketPopup = document.querySelector(".modal-basket");
+
+var basketClose = basketPopup.querySelector(".modal-close");
+
+var basketContinue = basketPopup.querySelector(".button-basket-continue");
+
+for(var i=0; i < basketLinks.length; i++){
+  basketLinks[i].addEventListener("click", function (evt){
+    evt.preventDefault();
+    basketPopup.classList.add("modal-show");
+  })
+};
+
+basketClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  basketPopup.classList.remove("modal-show");
+});
+
+basketContinue.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  basketPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (basketPopup.classList.contains("modal-show")) {
+      basketPopup.classList.remove("modal-show");
     }
   }
 });
