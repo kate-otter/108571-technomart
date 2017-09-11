3690c6626b84c2a483f20bf9f73b003d0cebe9a5
kate-otter <kate.otter@yandex.ru>
2017-09-11 18:07:35 +0500

    Исправленный js

--- js/script.js
+++ js/script.js
@@ -1,81 +1,91 @@
-var link = document.querySelector(".button-write-us");
-
+// модалка напишите нам
 var popup = document.querySelector(".modal-write-us");
 
-var close = popup.querySelector(".modal-close");
+if(popup != undefined){
+  var link = document.querySelector(".button-write-us");
+  var close = popup.querySelector(".modal-close");
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal-show");
-});
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
 
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
-      popup.classList.remove("modal-show");
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+  });
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+      }
     }
-  }
-});
+  });
+}
 
 
+
+// модалка - карта
 var mapLink = document.querySelector(".map");
 
-var mapPopup = document.querySelector(".modal-map");
+if(mapLink != undefined){
+  var mapPopup = document.querySelector(".modal-map");
 
-var mapClose = mapPopup.querySelector(".modal-close");
+  var mapClose = mapPopup.querySelector(".modal-close");
 
-mapLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.add("modal-show");
-});
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
 
-mapClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mapPopup.classList.remove("modal-show");
-});
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
 
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (mapPopup.classList.contains("modal-show")) {
-      mapPopup.classList.remove("modal-show");
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        mapPopup.classList.remove("modal-show");
+      }
     }
-  }
-});
+  });
+}
 
-var basketLinks = document.querySelectorAll(".button-buy");
+// модалка - "каталог" - купить 
 
 var basketPopup = document.querySelector(".modal-basket");
 
-var basketClose = basketPopup.querySelector(".modal-close");
+if(basketPopup != undefined){
+  
+  var basketLinks = document.querySelectorAll(".button-buy");
 
-var basketContinue = basketPopup.querySelector(".button-basket-continue");
+  var basketClose = basketPopup.querySelector(".modal-close");
 
-for(var i=0; i < basketLinks.length; i++){
-  basketLinks[i].addEventListener("click", function (evt){
+  var basketContinue = basketPopup.querySelector(".button-basket-continue");
+
+  for(var i=0; i < basketLinks.length; i++){
+    basketLinks[i].addEventListener("click", function (evt){
+      evt.preventDefault();
+      basketPopup.classList.add("modal-show");
+    })
+  };
+
+  basketClose.addEventListener("click", function (evt) {
     evt.preventDefault();
-    basketPopup.classList.add("modal-show");
-  })
-};
+    basketPopup.classList.remove("modal-show");
+  });
 
-basketClose.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  basketPopup.classList.remove("modal-show");
-});
+  basketContinue.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    basketPopup.classList.remove("modal-show");
+  });
 
-basketContinue.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  basketPopup.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (basketPopup.classList.contains("modal-show")) {
-      basketPopup.classList.remove("modal-show");
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (basketPopup.classList.contains("modal-show")) {
+        basketPopup.classList.remove("modal-show");
+      }
     }
-  }
-});
+  });
+}
